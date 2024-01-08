f4271f67efef804f5cba8206ab00d7b56c530b5d
Евгений Кутолин <dev9@max.id>
2024-01-08 15:37:38 +0400

    feat: added draft version initial stage project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
+import fs from 'fs';
+
 const program = new Command();
 
-program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
+const gendiff = (file1, file2) => {
+  console.log(file1, file2);
+};
 
 program
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --file <type>', 'output format');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --file <type>', 'output format')
+  .action((path1, path2) => {
+    const fileData = JSON.parse(fs.readFileSync(path1, 'utf-8'));
+    const fileData2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
+    const fileDataKeys = Object.keys(fileData);
+    const fileDataValues = Object.values(fileData);
+  });
 
 program.parse(process.argv);
